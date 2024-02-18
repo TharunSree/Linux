a213bb5b53163b817d0ed53b77e65f68ac63a661
Tharun Sreejith <73305454+TharunSree@users.noreply.github.com>
2024-02-18 10:46:22 +0530

    Update ins.sh

--- ins.sh
+++ ins.sh
@@ -1,17 +1,13 @@
-
 #!/bin/bash
 
 target=/mnt
-pacstrap -Syy
-pacstrap -i /mnt base base-devel linux linux-zen linux-firmware efibootmgr intel-ucode networkmanager
+pacstrap $target base base-devel linux linux-zen linux-firmware efibootmgr intel-ucode networkmanager
 
-genfstab -U -p /mnt >> /mnt/etc/fstab
+genfstab -U -p $target >> $target/etc/fstab
 
-arch-chroot /mnt <<EOF
-
+arch-chroot $target <<EOF
 ln -sf /usr/share/zoneinfo/Asia/Kolkata /etc/localtime
-
 hwclock --systohc --utc
 systemctl enable NetworkManager
-pacman -S --nonconfirm git
+pacman -S --noconfirm git
 EOF
